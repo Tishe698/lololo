d7e4be2946b795aaeb9a2062278bf70d834d0044
Your Name <poke37454@yandex.ru>
2023-06-01 20:41:21 +0300

    title logo

--- sait/urls.py
+++ sait/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from sait.views import *
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
     path('about_me/', page2, name='about_me')
 ]
