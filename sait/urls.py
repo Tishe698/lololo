94819a69ed83443a8887254962b3a2414dd043bc
Your Name <poke37454@yandex.ru>
2023-06-11 00:31:55 +0300

    title logo

--- sait/urls.py
+++ sait/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
+
 from django.urls import path
 from sait.views import *
 urlpatterns = [
     path('', index, name='home'),
-    path('about_me/', page2, name='about_me')
 ]
--- sait/views.py
+++ sait/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(req):
     return render(req, 'index.html')
-
-def page2(req):
-    return render(req,'about_me.html')
