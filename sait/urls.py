dacee9924810d8ede849fa7e9601373a90b79733
Your Name <poke37454@yandex.ru>
2023-06-15 01:38:08 +0300

    title logo

--- sait/urls.py
+++ sait/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from sait.views import *
 urlpatterns = [
     path('', index, name='home'),
+    path('about_me', about_me, name='about_me')
 ]
--- sait/views.py
+++ sait/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(req):
     return render(req, 'index.html')
+
+def about_me(req):
+    return render(req, 'about_me.html')
