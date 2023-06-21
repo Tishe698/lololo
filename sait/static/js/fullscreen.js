e3134e42107b1df9777477a3942b8bd590099316
Your Name <poke37454@yandex.ru>
2023-06-21 11:05:52 +0300

    title logo

--- sait/static/js/fullscreen.js
+++ sait/static/js/fullscreen.js
@@ -1,21 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var fullscreenLink = document.getElementById('fullscreen-link');
-
-  if (fullscreenLink) {
-    fullscreenLink.addEventListener('click', function(event) {
-      event.preventDefault();
-
-      // Check if the Fullscreen API is supported by the browser
-      if (document.documentElement.requestFullscreen) {
-        // Open the page in fullscreen mode
-        document.documentElement.requestFullscreen();
-      } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-        document.documentElement.mozRequestFullScreen();
-      } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-        document.documentElement.webkitRequestFullscreen();
-      } else if (document.documentElement.msRequestFullscreen) { // Internet Explorer and Edge
-        document.documentElement.msRequestFullscreen();
-      }
-    });
-  }
-});
+    window.onload = function () {
+        var element = document.documentElement;
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    };
