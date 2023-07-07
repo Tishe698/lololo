85bb6f193e3cf43de0b10d76f4e0b19ae7902d1a
Your Name <poke37454@yandex.ru>
2023-07-08 00:43:17 +0300

    title logo

--- sait/static/js/header.js
+++ sait/static/js/header.js
@@ -1,11 +1,12 @@
 const toglerBtn = document.querySelector('.togler_btn');
 const toglerBtnIcon = document.querySelector('.togler_btn i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
-const homeSection = document.querySelector('.home');
+
 
 toglerBtn.onclick = function () {
     dropDownMenu.classList.toggle('open');
     const isOpen = dropDownMenu.classList.contains('open');
     toglerBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
-    homeSection.classList.toggle('hidden');
+
 };
+
